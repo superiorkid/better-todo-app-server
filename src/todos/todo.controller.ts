c50e9ca137cb3f96aae9ec0f2083b7fd446ba740
superiorkid <bayiPetani@protonmail.com>
2023-01-20 17:00:02 +0800

    complete todo 🔥

--- src/todos/todo.controller.ts
+++ src/todos/todo.controller.ts
@@ -4,6 +4,7 @@ import {validationResult} from 'express-validator'
 import Todos from "./todo.model";
 import ITodo from "../types/ITodo";
 
+
 export const getAllTodo = async (req: Request, res:Response) => {
     try {
         const todos: Array<ITodo> = await Todos.find()
@@ -17,12 +18,12 @@ export const getAllTodo = async (req: Request, res:Response) => {
         res.status(500).json({
             code: 500,
             status: "INTERNAL_SERVER_ERROR",
-            message: "❌[error] : Something wrong!!"
+            message: "[❌] : Something wrong!!"
         })
     }
 }
 
-export const createNewTodo = (req: Request, res: Response) => {
+export const createNewTodo = async (req: Request, res: Response) => {
     const { title, todo } = req.body
     const errors = validationResult(req)
 
@@ -42,18 +43,115 @@ export const createNewTodo = (req: Request, res: Response) => {
             todo
         })
 
-        newTodo.save()
+        await newTodo.save()
         res.status(201).json({
             code: 201,
             status: 'CREATED',
-            message: "✅ Todo created successfylly"
+            message: "[✅] Todo created successfully"
         })
     } catch (error) {
         console.log(error)
         res.status(500).json({
             code: 500,
             status: 'INTERNAL_SERVER_ERROR',
-            message: "❌[error] : Something wrong!!"
+            message: "[❌] : Something wrong!!"
         })
     }
 }
+
+
+export const getTodoById = async (req: Request, res: Response) => {
+    const {id} = req.params
+
+    try {
+        await Todos.findOne({_id: id}).then((doc) => {
+            res.status(200).json({
+                code: 200,
+                status: "OK",
+                data: doc
+            })
+        }).catch((err) => {
+            res.status(404).json({
+                code: 404,
+                status: 'NOT_FOUND',
+                message: "[❌] : Todo not found"
+            })
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            code: 500,
+            status: 'INTERNAL_SERVER_ERROR',
+            message: "[❌] : Something wrong!!"
+        })
+    }
+}
+
+
+export const deleteTodo = async (req: Request, res: Response) => {
+    const {id} = req.params
+
+    try {
+        await Todos.findByIdAndDelete({_id: id}).then((doc) => {
+            res.status(200).json({
+                code: 200,
+                status: "OK",
+                message: "[✅] Todo deleted successfully"
+            })
+        }).catch((err) => {
+            res.status(404).json({
+                code: 404,
+                status: 'NOT_FOUND',
+                message: "[❌] : Todo not found"
+            })
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            code: 500,
+            status: 'INTERNAL_SERVER_ERROR',
+            message: "[❌] : Something wrong!!"
+        })
+    }
+}
+
+export const updateTodo = async (req: Request, res: Response) => {
+    const {id} = req.params
+    const todo: ITodo = {...req.body, updated_at: new Date}
+
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            code: 400,
+            status: 'BAD_REQUEST',
+            errors: errors.array()
+        })
+    }
+
+    try {
+        await Todos
+            .findByIdAndUpdate({_id: id}, todo)
+            .then((doc) => {
+                res.status(200).json({
+                    code: 200,
+                    status: "OK",
+                    message: "[✅] Todo update successfully"
+                })
+            })
+            .catch((err) => {
+                res.status(404).json({
+                    code: 404,
+                    status: 'NOT_FOUND',
+                    message: "[❌] : Todo not found"
+                })
+            })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            code: 500,
+            status: 'INTERNAL_SERVER_ERROR',
+            message: "[❌] : Something wrong!!"
+        })
+    }
+}
+
--- src/todos/todo.route.ts
+++ src/todos/todo.route.ts
@@ -1,5 +1,5 @@
 import express, {Router} from 'express'
-import {createNewTodo, getAllTodo} from "./todo.controller";
+import {createNewTodo, deleteTodo, getAllTodo, getTodoById, updateTodo} from "./todo.controller";
 import {body} from 'express-validator'
 
 const router = Router()
@@ -16,5 +16,20 @@ router.post(
         .withMessage("todo is required"),
     createNewTodo
 )
+router.get('/:id', getTodoById)
+router.delete('/:id', deleteTodo)
+router.put(
+    "/:id",
+    body("title")
+        .not()
+        .isEmpty()
+        .withMessage("title is required"),
+    body('todo')
+        .not()
+        .isEmpty()
+        .withMessage("todo is required"),
+    updateTodo
+)
+
 
 export default router
